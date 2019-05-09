ea08855a9c9b9039e3d84c21c8c4a73182cfebd0
Arthur Vinchon <arthur.vinchon@allegrodvt.com>
2019-05-09 15:52:15 +0200

    add command-line makefile

---
+++ command-line-requirements.mk
@@ -0,0 +1,14 @@
+include utils.mk
+
+this.command-line-requirements:=$(call get-my-dir)
+
+COMMAND_LINE_SRCS:=\
+	$(this.command-line-requirements)/command-line.cpp\
+
+COMMAND_LINE_INCLUDE_DIRECTORIES:=\
+
+COMMAND_LINE_INCLUDE_FILES:=\
+
+COMMAND_LINE_CFLAGS:=\
+
+COMMAND_LINE_LDFLAGS:=\
---
+++ utils.mk
@@ -0,0 +1,5 @@
+define get-my-dir
+$(patsubst %/,%,$(dir $(lastword $(MAKEFILE_LIST))))
+endef
+
+
