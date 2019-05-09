c01b18f91d45adc2f5a96a9b4fa1a0cdbce45aca
Arthur Vinchon <arthur.vinchon@allegrodvt.com>
2019-05-09 15:00:54 +0200

    remove namespace

--- command-line.hpp
+++ command-line.hpp
@@ -9,32 +9,28 @@
 #include <functional>
 #include <memory>
 
-namespace peanuts
-{
-  struct CommandLine{
-    ~CommandLine();
-    CommandLine();
+struct CommandLine{
+  ~CommandLine();
+  CommandLine();
 
-    CommandLine(CommandLine const&);
-    CommandLine& operator=(CommandLine const&);
+  CommandLine(CommandLine const&);
+  CommandLine& operator=(CommandLine const&);
 
-    CommandLine(CommandLine &&);
-    CommandLine& operator=(CommandLine &&);
+  CommandLine(CommandLine &&);
+  CommandLine& operator=(CommandLine &&);
 
-    void add_int_command(std::string name, std::string description, int &value);
-    void add_string_command(std::string name, std::string description, std::string &value);
-    void add_flag_command(std::string name, std::string description, bool &value, bool flag);
-    void add_user_command(std::string name, std::string description,
-        std::function<void(std::queue<std::string> &line)> parse);
-    void usage(std::ostream &stream) const;
-    void parse(std::queue<std::string> &line) const;
+  void add_int_command(std::string name, std::string description, int &value);
+  void add_string_command(std::string name, std::string description, std::string &value);
+  void add_flag_command(std::string name, std::string description, bool &value, bool flag);
+  void add_user_command(std::string name, std::string description,
+      std::function<void(std::queue<std::string> &line)> parse);
+  void usage(std::ostream &stream) const;
+  void parse(std::queue<std::string> &line) const;
 
-    private:
-    struct Implementation;
-    std::unique_ptr<Implementation> implementation;
-  };
+  private:
+  struct Implementation;
+  std::unique_ptr<Implementation> implementation;
+};
 
-
-} // namespace peanuts
 
 #endif /* __COMMAND_LINE__ */
