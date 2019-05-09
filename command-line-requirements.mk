0400cbb17429463d19bf07db6b80915e84f18210
Arthur Vinchon <arthur.vinchon@allegrodvt.com>
2019-05-09 16:01:06 +0200

    add namespace in makefile

--- command-line-requirements.mk
+++ command-line-requirements.mk
@@ -1,14 +1,14 @@
 include utils.mk
 
-this.command-line-requirements:=$(call get-my-dir)
+this.carryall-command-line-requirements:=$(call get-my-dir)
 
-COMMAND_LINE_SRCS:=\
-	$(this.command-line-requirements)/command-line.cpp\
+CARRYALL_COMMAND_LINE_SRCS:=\
+	$(this.carryall-command-line-requirements)/command-line.cpp\
 
-COMMAND_LINE_INCLUDE_DIRECTORIES:=\
+CARRYALL_COMMAND_LINE_INCLUDE_DIRECTORIES:=\
 
-COMMAND_LINE_INCLUDE_FILES:=\
+CARRYALL_COMMAND_LINE_INCLUDE_FILES:=\
 
-COMMAND_LINE_CFLAGS:=\
+CARRYALL_COMMAND_LINE_CFLAGS:=\
 
-COMMAND_LINE_LDFLAGS:=\
+CARRYALL_COMMAND_LINE_LDFLAGS:=\
