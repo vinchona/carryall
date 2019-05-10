4b1775d7410342564226e2a4250d02cefca4ad8d
Arthur Vinchon <arthur.vinchon@allegrodvt.com>
2019-05-10 11:13:04 +0200

    add carryall file in include dir

--- carryall-command-line-requirements.mk
+++ carryall-command-line-requirements.mk
@@ -3,9 +3,10 @@ include utils.mk
 this.carryall-command-line-requirements:=$(call get-my-dir)
 
 CARRYALL_COMMAND_LINE_SRCS:=\
-	$(this.carryall-command-line-requirements)/command-line.cpp\
+  $(this.carryall-command-line-requirements)/command-line.cpp\
 
 CARRYALL_COMMAND_LINE_INCLUDE_DIRECTORIES:=\
+  $(this.carryall-command-line-requirements)\
 
 CARRYALL_COMMAND_LINE_INCLUDE_FILES:=\
 
