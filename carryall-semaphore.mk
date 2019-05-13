dc179c7a5ab9b6d0565379fa08ffda6b600a0fe0
Arthur Vinchon <arthur.vinchon@allegrodvt.com>
2019-05-13 13:31:46 +0200

    add semaphore

---
+++ carryall-semaphore.mk
@@ -0,0 +1,14 @@
+include utils.mk
+
+this.carryall-semaphore-requirements:=$(call get-my-dir)
+
+CARRYALL_COMMAND_LINE_SRCS:=\
+
+CARRYALL_COMMAND_LINE_INCLUDE_DIRECTORIES:=\
+  $(this.carryall-semaphore-requirements)\
+
+CARRYALL_COMMAND_LINE_INCLUDE_FILES:=\
+
+CARRYALL_COMMAND_LINE_CFLAGS:=\
+
+CARRYALL_COMMAND_LINE_LDFLAGS:=\
---
+++ semaphore.hpp
@@ -0,0 +1,45 @@
+#pragma once
+
+#ifndef SEMAPHORE_HPP
+#define SEMAPHORE_HPP
+
+#include <condition_variable>
+#include <mutex>
+
+struct semaphore
+{
+  semaphore(size_t count = 0) :
+    count{count}
+  {
+  }
+
+  semaphore(const semaphore &&) = delete;
+  semaphore(semaphore &&) = delete;
+  semaphore & operator = (const semaphore &) = delete;
+  semaphore & operator = (semaphore &&) = delete;
+
+  ~semaphore() = default;
+
+  void signal()
+  {
+    std::unique_lock<std::mutex> lock(mutex);
+    ++count;
+    condition_variable.notify_one();
+  }
+
+  void wait()
+  {
+    std::unique_lock<std::mutex> lock(mutex);
+    condition_variable.wait(lock, [&] { return count > 0;
+                     });
+    --count;
+  }
+
+private:
+  std::mutex mutex;
+  std::condition_variable condition_variable;
+  size_t count;
+};
+
+
+#endif /* __SEMAPHORE__ */
