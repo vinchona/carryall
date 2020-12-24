dd212c09e56bbadc484d9bab9a3fe0d94897b663
Arthur Vinchon <vinchon.arthur@gmail.com>
2020-12-25 00:27:13 +0100

    reformat

--- tests/fuzzer-blocking-queue.cpp
+++ tests/fuzzer-blocking-queue.cpp
@@ -1,21 +1,21 @@
 #include <peanuts.hpp>
 
+#include <blocking-queue.hpp>
 #include <cstdio>
 #include <iostream>
-#include <blocking-queue.hpp>
 
 using namespace carryall;
 
 PEANUTS_FUZZ("BlockingQueue: Random number of inputs")
 {
-	 BlockingQueue<char> bloquing_queue;
-	 for (size_t size = 0; size < peanuts_size; size++)
-		 bloquing_queue.push(peanuts_data[size]);
+  BlockingQueue<char> bloquing_queue;
+  for (size_t size = 0; size < peanuts_size; size++)
+    bloquing_queue.push(peanuts_data[size]);
 
-	 for (size_t size = 0; size < peanuts_size; size++)
-	 {
-		 char value;
-		 bloquing_queue.pull(value);
-		 PEANUTS_IS_EQUAL_OR_ABORT(peanuts_data[size], value);
-	 }
+  for (size_t size = 0; size < peanuts_size; size++)
+  {
+    char value;
+    bloquing_queue.pull(value);
+    PEANUTS_IS_EQUAL_OR_ABORT(peanuts_data[size], value);
+  }
 }
--- tests/tester-blockingg-queue.cpp
+++ tests/tester-blockingg-queue.cpp
@@ -1,13 +1,9 @@
 #include <peanuts.hpp>
 
+#include <blocking-queue.hpp>
 #include <cstdio>
 #include <iostream>
-#include <blocking-queue.hpp>
-
 
 using namespace carryall;
 
-PEANUTS_TEST("BlockingQueue: Creation/Deletion")
-{ 
-  BlockingQueue<int> bloquing_queue;
-}
+PEANUTS_TEST("BlockingQueue: Creation/Deletion") { BlockingQueue<int> bloquing_queue; }
